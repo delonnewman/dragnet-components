e686b1141a976064a628dec16d767775a28530cc
Delon R. Newman <contact@delonnewman.name>
2026-02-16 14:05:25 -0500

    Get dev environment setup

--- src/dragnet/grid.ts
+++ src/dragnet/grid.ts
@@ -1,1 +1,5 @@
 import { Grid } from "@vaadin/grid";
+import { GridColumn } from "@vaadin/grid/src/vaadin-grid-column";
+
+window.customElements.define('dragnet-grid', Grid);
+window.customElements.define('dragnet-grid-column', GridColumn);
